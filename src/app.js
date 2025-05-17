2de07645ee12de7fd81e297bf5816f2460fd3127
braianCg <166315887+braianCg@users.noreply.github.com>
2025-05-17 19:33:31 -0300

    commit proyedcto final

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
-import express from "express";
-import passport from "passport";
-import passportConfig from "./config/passport/passport.js";
-import authRoutes from "./routes/auth.routes.js";
-import protectedRoutes from "./routes/protectedRoutes.js";
+import express from 'express';
+import envsConfig from './config/envs.config.js';
+import { connectMongoDB } from './config/nomgoDB.config.js';
+import routes from './routes/index.js';
+import cookieParser from 'cookie-parser';
+import passport from "./config/passport/passport.config.js";
+import cors from "cors";
 
 const app = express();
 
+connectMongoDB();
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.use(cookieParser());
+
 app.use(passport.initialize());
 
-app.use("/auth", authRoutes);
-app.use("/api", protectedRoutes);
+app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`));
+app.use("/api", routes);
+
+app.use((req, res) => {
+    res.status(404).json({ error: "Recurso no encontrado" });
+});
+
+app.listen(envsConfig.PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${envsConfig.PORT}`);
+
+})
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,53 +1,20 @@
-import express from "express";
-import passport from "passport";
-import { userModel } from "../persistence/mongo/dao/user.dao.js";
-import { hashPassword, comparePassword } from "../utils/hasPassword.js";
-import { generateToken } from "../utils/jwt.js";
+import { Router } from "express";
+import { authRole } from "../middlewares/authRole.middlewares.js";
+import { passportCall } from "../middlewares/passportCall.middleware.js";
+import { authController } from "../controllers/auth.controller.js";
+import { validateSchema } from "../middlewares/validateSchema.midelleware.js";
+import { registerSchema } from "../schemas/register.schema.js";
+import { loginSchema } from "../schemas/login.schema.js";
 
-const router = express.Router();
 
-router.post("/register", async (req, res) => {
-    const { first_name, last_name, email, age, password, role } = req.body;
+const router = Router();
 
-    try {
-        const existingUser = await userModel.findOne({ email });
-        if (existingUser) return res.status(400).json({ message: "El usuario ya está registrado" });
+router.post("/login", validateSchema({ body: loginSchema }), passportCall("login"), authController.login);
 
-        const hashedPassword = hashPassword(password);
-        const newUser = await userModel.create({
-            first_name,
-            last_name,
-            email,
-            age,
-            password: hashedPassword,
-            role,
-        });
+router.post("/register", validateSchema({ body: registerSchema }), passportCall("register"), authController.register)
 
-        res.status(201).json({ message: "Usuario registrado exitosamente", user: newUser });
-    } catch (error) {
-        res.status(500).json({ message: "Error al registrar usuario", error });
-    }
-});
+router.get("/current", passportCall("jwt"), authRole(["admin", "user"]), authController.current);
 
-router.post("/login", (req, res, next) => {
-    passport.authenticate("login", (err, user, info) => {
-        if (err) return res.status(500).json({ message: "Error interno del servidor" });
-        if (!user) return res.status(400).json({ message: info.message });
-
-        const token = generateToken({ id: user._id, email: user.email, role: user.role });
-
-        res.status(200).json({
-            message: "Inicio de sesión exitoso",
-            token,
-            user: {
-                id: user._id,
-                first_name: user.first_name,
-                last_name: user.last_name,
-                email: user.email,
-                role: user.role,
-            },
-        });
-    })(req, res, next);
-});
+router.get("/logout", passportCall("jwt"), authController.logout);
 
 export default router;
